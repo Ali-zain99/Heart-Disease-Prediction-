e894447e8e0fac1f5baff5197dd453f2f44d52a0
ali zain <93091424+Ali-zain99@users.noreply.github.com>
2024-05-13 07:49:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ df = pd.concat([input_df, heart_dataset], axis=0)
 # df = pd.get_dummies(df, columns=['male', 'prevalentStroke', 'prevalentHyp', 'diabetes'])
 
 # Take only the first row (user input)
+df=df[:1]
 df=df.drop([ 'cigsPerDay','BPMeds'], axis=1)
 imputer = SimpleImputer(strategy='mean')
 imputer.fit(df)
